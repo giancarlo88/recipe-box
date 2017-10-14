443ae08a1810164e2a0fdfde3afae3bcc2839307
Giancarlo Galliani Pecchia <giancarlo.pecchia@arcadiagroup.co.uk>
2017-10-14 14:50:13 +0100

    Initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+const Header = styled.header`
+background-color: rgba(125, 30, 50, .5);
+color: green;
+`
+
+export default () => (
+  <Header>
+    <h1 className="App-title">Welcome to React</h1>
+  </Header>
+)
